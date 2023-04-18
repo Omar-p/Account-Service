03dd6bf99468fc0941e45ee551bb68e7a80b5c7b
Omar Shabaan <omar.plus.077@gmail.com>
2023-04-19 00:56:45 +0200

    add validation annotation for registration request fields

--- src/main/java/com/example/accountservice/registration/RegistrationRequest.java
+++ src/main/java/com/example/accountservice/registration/RegistrationRequest.java
@@ -1,4 +1,14 @@
 package com.example.accountservice.registration;
 
-public record RegistrationRequest(String name, String lastName, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record RegistrationRequest(
+        @NotNull @NotEmpty String name,
+        @NotNull @NotEmpty String lastname,
+        @NotNull @NotEmpty @Email
+        String email,
+        @NotNull @NotEmpty String password
+) {
 }
